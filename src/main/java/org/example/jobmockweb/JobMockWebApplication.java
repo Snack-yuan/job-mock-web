1518d9cb15854a663fbe48c2456002890eef9428
yuan <2206529229@qq.com>
2026-01-18 16:25:50 +0800

    1

--- src/main/java/org/example/jobmockweb/JobMockWebApplication.java
+++ src/main/java/org/example/jobmockweb/JobMockWebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobMockWebApplication {
 
     public static void main(String[] args) {
+        System.out.println("Starting JobMockWebApplication");
         SpringApplication.run(JobMockWebApplication.class, args);
     }
 
